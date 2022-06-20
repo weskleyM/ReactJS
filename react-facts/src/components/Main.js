403f0fad893a9b54fdce83fddb4501b0083b87a9
weskleyM <weskley512@gmail.com>
2022-06-20 17:17:43 -0300

    React Facts App adicionado

---
+++ react-facts/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts About React</h1>
+            <ul className="main--elemts">
+                <li>Was first released in 2013</li>
+                <li>Was originaly created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintened by Facebook</li>
+                <li>Power thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    );
+}
