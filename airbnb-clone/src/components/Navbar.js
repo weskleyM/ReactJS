55672f465db80d70a31a7f9391e0be0c4429fb3e
weskleyM <weskley512@gmail.com>
2022-06-21 11:46:36 -0300

    airbnb-clone adicionado a ReactJS

---
+++ airbnb-clone/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "../images/abnb-logo.png";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={Logo} />
+        </nav>
+    );
+}
