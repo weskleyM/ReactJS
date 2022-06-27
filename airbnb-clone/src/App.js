5c4204bde96e583080fe1f4f900db3281d48d1c0
weskleyM <weskley512@gmail.com>
2022-06-27 20:30:39 -0300

    Card from airbnb updated

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 // import Hero from "./components/Hero";
 import Card from "./components/Card";
+import Card_01 from "./images/card_01.jpg";
 
 export default function App() {
     return (
@@ -9,10 +10,10 @@ export default function App() {
             <Navbar />
             {/* <Hero /> */}
             <Card
-                img="card_01.jpg"
+                img={Card_01}
                 rating="5.0"
                 reviewCount={6}
-                country="USA"
+                country=" USA"
                 title="Life lessons with Katie Zarefas"
                 price={136}
             />
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Star from "../images/star.png";
 
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} alt="card" />
+            <img src={props.img} alt="card" />
             <div className="card--stats">
-                <img className="star--icon" src="../images/star.png" alt="star" />
+                <img className="star--icon" src={Star} alt="star" />
                 <span>{props.rating}</span>
-                <span className="gray">(6) * </span>
-                <span className="gray">USA</span>
+                <span className="gray">({props.reviewCount}) * </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life lessons with Katie Zarefas</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     );
 }
+
+// 3h50m
