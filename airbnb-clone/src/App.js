cfb5f2662915b4787e1c953cb805647c4e6c1f8a
weskleyM <weskley512@gmail.com>
2022-06-27 16:56:53 -0300

    App from airbnb-clone update

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
 import Card from "./components/Card";
+import Card_01 from "./images/card_01.jpg"
+import Card_02 from "./images/card_02.jpg"
+import Card_03 from "./images/card_03.jpg"
+import Star from "./images/star.png"
 
 export default function App() {
     return (
         <div>
-            <Navbar />
+            {/* <Navbar /> */}
             {/* <Hero /> */}
-            <Card />
+            <Card card={Card_01} star={Star} />
+            <Card card={Card_02} star={Star} />
+            <Card card={Card_03} star={Star} />
         </div>
     );
 }
