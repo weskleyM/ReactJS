bab22b6c47bfe817128b37e72c45d1f53e8e01f3
weskleyM <weskley512@gmail.com>
2022-06-28 13:27:27 -0300

    updated

--- Projects/abnb-clone/components/Card.js
+++ Projects/abnb-clone/components/Card.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Card() {
     return (
         <div>
-            
+            <img src="../images/card-01.png" alt="" />
+            <div>
+                <img src="../images/star.png" alt="" />
+            </div>
         </div>
     );
 }
